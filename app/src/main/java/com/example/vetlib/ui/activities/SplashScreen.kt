2ad524c8cdebd87f718ff489d8efdb308ca4aec0
eelba <eliran.logicode@gmail.com>
2020-06-27 17:35:41 +0200

    - Project creation - Packaging - FirstGit

---
+++ app/src/main/java/com/example/vetlib/ui/activities/SplashScreen.kt
@@ -0,0 +1,17 @@
+package com.example.vetlib.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vetlib.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
---
+++ app/src/main/java/com/example/vetlib/utils/Utils.kt
@@ -0,0 +1,5 @@
+package com.example.vetlib.utils
+
+/**
+ * Created by Eliran Elbaz on 27-Jun-20.
+ */
