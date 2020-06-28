9978c0b619f6832d7ba3629819367ec4af595b78
eelba <eliran.logicode@gmail.com>
2020-06-28 13:33:32 +0200

    - Begin of SplashScreen.kt design with logo and text - Added an horizontal progressBar to SplashScreen.kt - Added new dependencies - Added an abstract (object) Util class - Added supports for project in Gradle and Java 1.8

--- app/src/main/java/com/example/vetlib/ui/activities/SplashScreen.kt
+++ app/src/main/java/com/example/vetlib/ui/activities/SplashScreen.kt
@@ -1,8 +1,17 @@
 package com.example.vetlib.ui.activities
 
+import android.app.Activity
+import android.app.ActivityOptions
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.example.vetlib.R
+import com.example.vetlib.utils.Utils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +22,37 @@ class SplashScreen : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
+        if (!Utils.isInternetAvailable(this)) { // Verify Network connectivity
+            displayMobileDataSettingsDialog(this, this)
+        } else if (false){ // Verify if using last version "!isAppLastVersion()"
+            //TODO: ask for update  or mention it
+        }
+        else {  // Load the right activity
+        }
+
+    }
+
+
+    private fun intentActivity(classname: Class<*>) {
+        val intent = Intent(this, classname)
+        startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(this).toBundle())
+    }
+
+
+    // ---------------------------
+    // NETWORK CONNECTIVITY Dialog
+    // ----------------------------
+    private fun displayMobileDataSettingsDialog(activity: Activity?, context: Context): AlertDialog? {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("No Internet")
+        builder.setMessage("Please enable network access")
+        builder.setPositiveButton("Continue") {
+                dialog, which -> finish()
+            val intent = Intent(context, this::class.java)
+            context.startActivity(intent)
+            dialog.cancel()
+        }
+        builder.show()
+        return builder.create()
     }
 }
--- app/src/main/java/com/example/vetlib/utils/Utils.kt
+++ app/src/main/java/com/example/vetlib/utils/Utils.kt
@@ -1,5 +1,40 @@
 package com.example.vetlib.utils
 
+import android.content.ContentValues
+import android.content.Context
+import android.net.ConnectivityManager
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
 /**
  * Created by Eliran Elbaz on 27-Jun-20.
  */
+object Utils{
+
+
+    /****************************
+     * Network Availability
+     ****************************/
+
+    fun isInternetAvailable(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        Log.d(ContentValues.TAG, "isInternetAvailable: $activeNetworkInfo")
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected
+    }
+
+    /****************************
+     * Firebase Instance
+     ****************************/
+    fun isCurrentUserLogged(): Boolean? {
+        return this.getCurrentUser() != null
+    }
+
+    fun getCurrentUser(): FirebaseUser? {
+        return FirebaseAuth.getInstance().currentUser
+    }
+
+}
